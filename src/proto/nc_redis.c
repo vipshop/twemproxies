e893dea71cc586280e73b040d0873d723e7f2a97
deep011 <diguo58@gmail.com>
2016-08-03 16:58:18 +0800

    fix a bug about memory leak for mset command

--- src/proto/nc_redis.c
+++ src/proto/nc_redis.c
@@ -2327,6 +2327,8 @@ redis_copy_bulk(struct msg *dst, struct msg *src)
             mbuf_remove(&src->mhdr, mbuf);
             if (dst != NULL) {
                 mbuf_insert(&dst->mhdr, mbuf);
+            } else {
+                mbuf_put(mbuf);
             }
             len -= mbuf_length(mbuf);
             mbuf = nbuf;
